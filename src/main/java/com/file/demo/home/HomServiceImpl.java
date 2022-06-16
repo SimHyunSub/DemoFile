c8f05357cacf2005f9c40ddf18a0508bdfacde62
sseob <orolsyeo@gmail.com>
2022-06-16 17:52:06 +0900

    how many faster ?

--- src/main/java/com/file/demo/home/HomServiceImpl.java
+++ src/main/java/com/file/demo/home/HomServiceImpl.java
@@ -1,37 +1,28 @@
 package com.file.demo.home;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.RandomAccessFile;
-import java.io.UnsupportedEncodingException;
+import com.file.demo.home.aspect.LogExecutionTime;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
 import java.net.URLEncoder;
 import java.nio.ByteBuffer;
 import java.nio.channels.Channels;
 import java.nio.channels.FileChannel;
+import java.nio.channels.SeekableByteChannel;
 import java.nio.channels.WritableByteChannel;
-
-import javax.servlet.ServletOutputStream;
-import javax.servlet.http.HttpServletResponse;
-
-import org.springframework.stereotype.Service;
-
-import com.file.demo.home.aspect.LogExecutionTime;
-
-import lombok.extern.slf4j.Slf4j;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 @Slf4j
 @Service
 public class HomServiceImpl implements HomeService {
 	
-	private final String separator = File.separator;
-	private final String FILE_PATH = "C:" + separator + "DEV" + separator + "testResources"+ separator;
+	@Value("${user.file.path}")
+	private String FILE_PATH;
 
 	@LogExecutionTime
 	@Override
@@ -53,16 +44,13 @@ public class HomServiceImpl implements HomeService {
 		case DEFAULT:
 			fileStream(response, file);
 			break;
-		case DATA_STREAM:
-			dataStream(response, file);
-			break;
 		case BUFFERED_STREAM:
 			bufferedStream(response, file);
 			break;
-		case BYTE_BUFFER:
+		case CHANNEL:
 			fileChannel(response, file);
 			break;
-		case BYTE_BUFFER_RANDOM_ACCESS:
+		case CHANNEL_RANDOM_ACCESS:
 			fileChannelRandomAccess(response, file);
 			break;
 		default:
@@ -70,19 +58,6 @@ public class HomServiceImpl implements HomeService {
 		}
 	}
 
-	public void dataStream(HttpServletResponse response, FileVO file) {
-		
-		try (
-				DataInputStream is = new DataInputStream(new FileInputStream(FILE_PATH + file.getFileName()));
-				DataOutputStream os = new DataOutputStream(response.getOutputStream()); 
-		) {
-			readWrite(is, os, file);
-			
-		} catch (Exception e) {
-			log.error(e.getMessage(), e);
-		}
-	}
-	
 	public void fileStream(HttpServletResponse response, FileVO file) {
 		
 		try (
@@ -99,7 +74,7 @@ public class HomServiceImpl implements HomeService {
 	public void bufferedStream(HttpServletResponse response, FileVO file) {
 		
 		try (
-				BufferedInputStream is = new BufferedInputStream( new FileInputStream(FILE_PATH + file.getFileName()));
+				BufferedInputStream is = new BufferedInputStream(Files.newInputStream(Paths.get(FILE_PATH + file.getFileName())));
 				BufferedOutputStream os = new BufferedOutputStream(response.getOutputStream());
 		) {
 			readWrite(is, os, file);
@@ -109,9 +84,9 @@ public class HomServiceImpl implements HomeService {
 	}
 	
 	public void fileChannel(HttpServletResponse response, FileVO file) {
-		
+
 		try (
-				FileChannel channel = new FileInputStream(FILE_PATH + file.getFileName()).getChannel();
+				SeekableByteChannel channel = Files.newByteChannel(Paths.get(FILE_PATH + file.getFileName()));
 				WritableByteChannel outChannel = Channels.newChannel(response.getOutputStream());
 		) {
 
@@ -127,8 +102,6 @@ public class HomServiceImpl implements HomeService {
 		    	buffer.clear();
 			}
 		    
-		} catch (IOException e) {
-			log.error(e.getMessage(), e);
 		} catch (Exception e) {
 			log.error(e.getMessage(), e);
 		}
@@ -154,7 +127,6 @@ public class HomServiceImpl implements HomeService {
 				outChannel.write(buffer);
 				buffer.clear();
 			}
-			
 		} catch (IOException e) {
 			log.error(e.getMessage(), e);
 		} catch (Exception e) {
--- src/main/java/com/file/demo/home/enm/FileDownLoadEnum.java
+++ src/main/java/com/file/demo/home/enm/FileDownLoadEnum.java
@@ -2,8 +2,7 @@ package com.file.demo.home.enm;
 
 public enum FileDownLoadEnum {
 	DEFAULT
-	,DATA_STREAM
-	,BYTE_BUFFER
-	,BYTE_BUFFER_RANDOM_ACCESS
 	,BUFFERED_STREAM
+	,CHANNEL
+	,CHANNEL_RANDOM_ACCESS
 }
